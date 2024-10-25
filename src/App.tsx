c90404d796c734005f19466d5dc2efd7ddac0f3c
StefanoDaniello <stefanodaniello68@gmail.com>
2024-10-25 11:50:51 +0200

    fix i am super syan

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Scheduler } from "@aldabil/react-scheduler";
 import { it } from "date-fns/locale";
+import { Button } from "@mui/material";
 
 function App() {
   const events: any[] = [
@@ -23,11 +24,12 @@ function App() {
         view="week"
         events={events}
         locale={it}
+        hourFormat="24"
         month={{
-          weekDays: [0, 1, 2, 3, 4, 5, 6], // Order of days starting from Monday
-          weekStartOn: 1, // Start the week on Monday
-          startHour: 0, // Start hour for the scheduler
-          endHour: 24, // End hour for the scheduler
+          weekDays: [0, 1, 2, 3, 4, 5, 6], // Mostra tutti i giorni della settimana, ma abilitati solo quelli della settimana corrente
+          weekStartOn: 1, // Inizio della settimana di lunedì
+          startHour: 0, // Ora di inizio per il planner
+          endHour: 24, // Ora di fine per il planner
         }}
         week={{
           weekDays: [0, 1, 2, 3, 4, 5, 6],
@@ -35,8 +37,45 @@ function App() {
           startHour: 0,
           endHour: 24,
           step: 60,
-          navigation: true,
-          disableGoToDay: false,
+          cellRenderer: ({ height, start, onClick, ...props }) => {
+            // Ottieni la data e l'ora correnti
+            const now = new Date();
+
+            // Crea una nuova data senza ore, minuti, secondi e millisecondi
+            const currentTime = new Date(
+              now.getFullYear(),
+              now.getMonth(),
+              now.getDate(),
+              now.getHours() - 1,
+              now.getMinutes()
+            );
+
+            // Controlla se l'orario dell'evento è passato
+            const disabled = start.getTime() < currentTime.getTime(); // start è in millisecondi
+
+            const restProps = disabled ? {} : props;
+
+            return (
+              console.log({ start, currentTime }),
+              (
+                <Button
+                  style={{
+                    height: "100%",
+                    background: disabled ? "#eee" : "transparent",
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={() => {
+                    if (disabled) {
+                      return alert("Opss");
+                    }
+                    onClick();
+                  }}
+                  disableRipple={disabled}
+                  {...restProps}
+                ></Button>
+              )
+            );
+          },
         }}
         navigation={true}
       />
